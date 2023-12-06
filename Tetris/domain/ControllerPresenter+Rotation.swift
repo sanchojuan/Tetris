f5e6d9d24bfc4a08d1f38c63ffaabe93f224fe38
Juan <unify.app.and@gmail.com>
2023-12-06 13:54:27 +0100

    BlueRicky now can rotate

--- Tetris/domain/ControllerPresenter+Rotation.swift
+++ Tetris/domain/ControllerPresenter+Rotation.swift
@@ -22,6 +22,9 @@ extension ControllerPresenter {
             case Piece.orangeRicky:
                 rotateOrangeRicky()
                 break
+            case Piece.blueRicky:
+                rotateBlueRicky()
+                break
             default:
                 break
             }
@@ -41,7 +44,9 @@ extension ControllerPresenter {
                 if currentPieceRotation == 0 { currentPieceRotation = 90 }
                 else { currentPieceRotation = 0 }
             }
-            else if currentPieceType == Piece.orangeRicky {
+            else if currentPieceType == Piece.orangeRicky ||
+                    currentPieceType == Piece.blueRicky {
+                
                 if currentPieceRotation == 0 { currentPieceRotation = -90 }
                 else if currentPieceRotation == -90 { currentPieceRotation = -180 }
                 else if currentPieceRotation == -180 { currentPieceRotation = -270 }
@@ -163,18 +168,62 @@ extension ControllerPresenter {
             newPosition[3][1] -= 2
         }
         else if currentPieceRotation == -180 {
-            print(currentPiecePosition)
             newPosition[0][0] += 1
             newPosition[0][1] += 1
             newPosition[1][1] -= 2
             newPosition[2][0] += 1
             newPosition[2][1] -= 1
             newPosition[3][0] += 2
-            print(newPosition)
         }
         else if currentPieceRotation == -270 {
             newPosition[0][0] -= 2
             newPosition[0][1] += 1
+            newPosition[1][0] += 1
+            newPosition[2][1] += 1
+            newPosition[3][0] -= 1
+            newPosition[3][1] += 2
+        }
+        changes.append((currentPiecePosition[0][0], currentPiecePosition[0][1], 0))
+        changes.append((currentPiecePosition[1][0], currentPiecePosition[1][1], 0))
+        changes.append((currentPiecePosition[2][0], currentPiecePosition[2][1], 0))
+        changes.append((currentPiecePosition[3][0], currentPiecePosition[3][1], 0))
+        changes.append((newPosition[0][0], newPosition[0][1], 1))
+        changes.append((newPosition[1][0], newPosition[1][1], 1))
+        changes.append((newPosition[2][0], newPosition[2][1], 1))
+        changes.append((newPosition[3][0], newPosition[3][1], 1))
+        
+        tryRotation(newPosition: newPosition, changes: changes)
+    }
+    
+    func rotateBlueRicky() {
+        var newPosition = currentPiecePosition
+        var changes: [(r:Int, c:Int, value:Int)] = []
+        
+        if currentPieceRotation == 0 {
+            newPosition[0][0] += 2
+            newPosition[1][0] += 1
+            newPosition[1][1] += 1
+            newPosition[3][0] -= 1
+            newPosition[3][1] -= 1
+        }
+        else if currentPieceRotation == -90 {
+            newPosition[0][0] -= 1
+            newPosition[0][1] += 2
+            newPosition[1][0] -= 2
+            newPosition[1][1] += 1
+            newPosition[2][0] -= 1
+            newPosition[3][1] -= 1
+        }
+        else if currentPieceRotation == -180 {
+            newPosition[0][0] -= 1
+            newPosition[0][1] -= 1
+            newPosition[1][1] -= 2
+            newPosition[2][0] += 1
+            newPosition[2][1] -= 1
+            newPosition[3][0] += 2
+        }
+        else if currentPieceRotation == -270 {
+            newPosition[0][1] -= 1
             newPosition[1][0] += 1
             newPosition[2][1] += 1
             newPosition[3][0] -= 1
